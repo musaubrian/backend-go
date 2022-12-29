898628af2b62d83807a09e82540e31c0dee66f4e
musaubrian <musaubrian15@gmail.com>
2022-12-29 11:18:31 +0300

    chore: test out mux for routing

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main()  {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "you have requested, %s", r.URL.Path)
-    })
+    r := mux.NewRouter()
 
-    http.ListenAndServe(":80", nil)
+    r.HandleFunc("/books/{title}", func(w http.ResponseWriter, r *http.Request) {
+        vars := mux.Vars(r)
+        title := vars["title"]
+
+        fmt.Fprintf(w, "you have requested  the book %s\n", title)
+    })
+    http.ListenAndServe(":80",r)
 }
