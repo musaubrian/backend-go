697940034221d31bdec54d8897d0bd0f5d6d7702
musaubrian <musaubrian15@gmail.com>
2022-12-31 22:06:57 +0300

    chore: generate random string for the url

---
+++ utils/generateUrl.go
@@ -0,0 +1,14 @@
+package utils
+
+import "math/rand"
+
+var collection = []rune("0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM")
+func GenerateUrl()  string{
+    str := make([]rune)
+    
+    for i := range str {
+        str[i] = collection[rand.Intn(len(collection))]
+    }
+
+    return string(str)
+}
